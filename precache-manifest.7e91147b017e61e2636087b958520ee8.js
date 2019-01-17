8a43959ad0af849d8bc73c2ed5aed5307d72157f
tokyosuite <helenyueyu@gmail.com>
2019-01-17 10:26:06 -0800

    Updates

---
+++ precache-manifest.7e91147b017e61e2636087b958520ee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ae64b59a7ce9c8048f9",
+    "url": "/calculator-app/static/css/main.3e2270da.chunk.css"
+  },
+  {
+    "revision": "9ae64b59a7ce9c8048f9",
+    "url": "/calculator-app/static/js/main.9ae64b59.chunk.js"
+  },
+  {
+    "revision": "ed8c84cbbec32d54ce18",
+    "url": "/calculator-app/static/js/1.ed8c84cb.chunk.js"
+  },
+  {
+    "revision": "0017109f1eb89fac3320",
+    "url": "/calculator-app/static/js/runtime~main.0017109f.js"
+  },
+  {
+    "revision": "bb9b7696cb7f3267746f1ca1d2bf625b",
+    "url": "/calculator-app/index.html"
+  }
+];
